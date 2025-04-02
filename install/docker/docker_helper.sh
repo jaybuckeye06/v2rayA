40c068dcd70699d6d0964d21b469431845c78777
wasd13579 <38148063+wasd13579@users.noreply.github.com>
2025-04-02 13:27:34 +0800

    Update docker_helper.sh
    Signed-off-by: wasd13579 <38148063+wasd13579@users.noreply.github.com>

--- install/docker/docker_helper.sh
+++ install/docker/docker_helper.sh
@@ -24,7 +24,8 @@ esac
 mkdir -p build && cd build || exit
 wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-$v2ray_arch.zip
 wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-$v2ray_arch.zip
-wget https://github.com/wasd13579/v2rayA/releases/download/latest/v2raya
+#wget https://github.com/wasd13579/v2rayA/releases/download/latest/v2raya
+wget https://github.com/wasd13579/v2rayA/releases/download/vRealv2rayAVersion/v2raya_linux_"$v2raya_arch"_Realv2rayAVersion
 unzip v2ray-linux-"$v2ray_arch".zip -d v2ray
 install ./v2ray/v2ray /usr/local/bin/v2ray
 unzip Xray-linux-"$v2ray_arch".zip -d xray
